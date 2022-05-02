2fe28a2af1e20a91217c23b27415815419051de1
saheed007 <saheedoladejo007@gmail.com>
2022-05-02 22:16:44 +0100

    Add files via upload

---
+++ function.py
@@ -0,0 +1,16 @@
+# Functiona are very useful in python codes as they help you ensure reusability of lines of codes without having to retype them every single time.
+# This practice is to explore how to create functions and check out some examples of how to use them
+
+
+def math(a,b,c):
+    print("Linear Equation: ", a, "x +", b, "y =", c)
+    print("Quadratic Equation: ", a,"x^2 +", b, "x +", c,"= 0")
+    return 
+
+math(2,3,4)
+
+v1 = input("Input Value1: ")
+v2 = input("Input Value2: ")
+v3 = input("Input Value3: ")
+
+math(v1, v2, v3)
