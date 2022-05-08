5b9cd428edf2b987361dda2ff47ca6c7c6d3dca1
saheed007 <saheedoladejo007@gmail.com>
2022-05-08 18:09:21 +0100

    Add files via upload

---
+++ number graphics.py
@@ -0,0 +1,15 @@
+eight= ' ______\n|      |\n|______|\n|      |\n|______|\n'
+one='\n       |\n       |\n       |\n       |'
+two= ' ______\n       |\n ______|\n|       \n|______\n'
+three=' ______\n       |\n ______|\n       |\n ______|\n'
+five=' ______\n|       \n|______ \n       |\n ______|\n'
+six=' ______\n|       \n|______ \n|      |\n|______|\n'
+seven=' ______\n       |\n       |\n       |\n       |\n'
+nine=' ______\n|      |\n|______|\n       |\n ______|\n'
+four='       \n|      |\n|______|\n       |\n       |\n'
+zero=' ______\n|      |\n|      |\n|      |\n|______|\n'
+
+names=[one, two, three, four, five, six, seven, eight, nine]
+
+for x in names:
+   print(x)
